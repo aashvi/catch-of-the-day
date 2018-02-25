d45981206c8343ef2bb4e0c6cdc42a8f03da1090
Ayush Agrawal <ayushagrawal@ayush.local>
2018-02-25 17:38:34 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// let's go!
+import React from 'react';
+import './css/style.css';
+
+
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
